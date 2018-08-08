c536a4c1d3a6ae14ed8430cfe8f7e5b6fd4e5bfe
Jason Howk <jason.howk@subaquatic.net>
2018-08-07 20:29:39 -0600

    Initial Commit

---
+++ News Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News Feed
+//
+//  Created by Jason Howk on 8/7/18.
+//  Copyright © 2018 Rude Tie, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
